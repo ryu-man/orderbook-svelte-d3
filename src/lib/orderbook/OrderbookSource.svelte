a18afa5b10f885de4a72440ece89e00905ad5e04
abdelhalim <rioman19@gmail.com>
2023-08-14 01:26:51 +0100

    feat: auto reconnect

--- src/lib/orderbook/OrderbookSource.svelte
+++ src/lib/orderbook/OrderbookSource.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
+	import { documentVisibilityStore } from 'svelte-legos';
 	import Orderbook from './Orderbook.svelte';
 	import { messages } from './store';
+	import { onMount } from 'svelte';
+
+	const visibility$ = documentVisibilityStore();
 
 	export let productId: string;
 	export let url: string;
@@ -11,7 +15,19 @@
 	export let y = 0;
 	export let thresholds: number[];
 
-	const { asks$, bids$ } = messages(url, productId);
+	const { asks$, bids$, isClosed, reconnect } = messages(url, productId);
+
+	onMount(() =>
+		visibility$.subscribe((value) => {
+			if (value === 'visible') {
+				// Check if websocket is closed
+				if (isClosed()) {
+					// Then reconnect the websocket
+					reconnect();
+				}
+			}
+		})
+	);
 </script>
 
 <Orderbook {width} {height} {x} {y} name={productId} {asks$} {bids$} {thresholds} />
--- src/lib/orderbook/store.ts
+++ src/lib/orderbook/store.ts
@@ -5,183 +5,81 @@ export function messages(url: string, productId = 'BTC-USD') {
 	const bids$ = writable([]);
 	const asks$ = writable([]);
 
-	const ws = new WebSocket(url);
+	let ws = connect(url);
 
-	ws.onopen = () => {
-		ws.send(
-			JSON.stringify({
-				type: 'subscribe',
-				product_ids: [productId],
-				channels: ['level2_batch']
-			})
-		);
-	};
+	function connect(url: string) {
+		const ws = new WebSocket(url);
+		ws.onopen = () => {
+			ws.send(
+				JSON.stringify({
+					type: 'subscribe',
+					product_ids: [productId],
+					channels: ['level2_batch']
+				})
+			);
+		};
 
-	ws.onmessage = (e) => {
-		const raw = JSON.parse(e.data);
+		ws.onmessage = (e) => {
+			const raw = JSON.parse(e.data);
 
-		switch (raw.type) {
-			case 'snapshot': {
-				const data = parseSnapshot(raw);
+			switch (raw.type) {
+				case 'snapshot': {
+					const data = parseSnapshot(raw);
 
-				bids$.set(data.bids);
-				asks$.set(data.asks);
+					bids$.set(data.bids);
+					asks$.set(data.asks);
 
-				break;
+					break;
+				}
+				case 'l2update': {
+					const data = {
+						type: raw.type,
+						product_id: raw.product_id,
+						changes: raw.changes.map((change) => [
+							change[0],
+							parseFloat(change[1]),
+							parseFloat(change[2])
+						]),
+						time: new Date(raw.time)
+					} as Update;
+
+					// sync changes
+					data.changes.forEach((change) => {
+						const type = change[0];
+						if (type === 'buy') {
+							bids$.update((val) => sync(val, change));
+						} else {
+							asks$.update((val) => sync(val, change));
+						}
+					});
+					break;
+				}
 			}
-			case 'l2update': {
-				const data = {
-					type: raw.type,
-					product_id: raw.product_id,
-					changes: raw.changes.map((change) => [
-						change[0],
-						parseFloat(change[1]),
-						parseFloat(change[2])
-					]),
-					time: new Date(raw.time)
-				} as Update;
+		};
 
-				// sync changes
-				data.changes.forEach((change) => {
-					const type = change[0];
-					if (type === 'buy') {
-						bids$.update((val) => sync(val, change));
-					} else {
-						asks$.update((val) => sync(val, change));
-					}
-				});
-				break;
-			}
+		ws.onerror = (err) => {
+			console.error(err);
+		};
+
+		return ws;
+	}
+
+	function reconnect() {
+		if (ws.readyState !== ws.CLOSED) {
+			ws.close();
 		}
-	};
+		ws = connect(url);
+	}
 
-	ws.onerror = (err) => {
-		console.error(err);
-	};
+	function isClosed() {
+		return ws.readyState === ws.CLOSED;
+	}
 
-	// const feeds$ = readable({}, (set) => {
-	// 	ws.onmessage = (e) => {
-	// 		const raw = JSON.parse(e.data);
-	// 		set(raw);
+	function disconnect() {
+		return ws.close();
+	}
 
-	// 		switch (raw.type) {
-	// 			case 'snapshot': {
-	// 				const data = parseSnapshot(raw);
-
-	// 				bids$.set(data.bids);
-	// 				asks$.set(data.asks);
-	// 				break;
-	// 			}
-	// 			case 'l2update': {
-	// 				const data = {
-	// 					type: raw.type,
-	// 					product_id: raw.product_id,
-	// 					changes: raw.changes.map((change) => [
-	// 						change[0],
-	// 						parseFloat(change[1]),
-	// 						parseFloat(change[2])
-	// 					]),
-	// 					time: new Date(raw.time)
-	// 				} as Update;
-
-	// 				// sync changes
-	// 				data.changes.forEach((change) => {
-	// 					const type = change[0];
-	// 					if (type === 'buy') {
-	// 						bids$.update((val) => sync(val, change));
-	// 					} else {
-	// 						asks$.update((val) => sync(val, change));
-	// 					}
-	// 				});
-	// 				break;
-	// 			}
-	// 		}
-	// 	};
-
-	// 	return () => {
-	// 		ws.close();
-	// 	};
-	// });
-
-	// const subscriptions$ = derived(
-	// 	feeds$,
-	// 	(data, set) => {
-	// 		if (data.type === 'subscriptions') {
-	// 			set(data);
-	// 		}
-	// 	},
-	// 	{}
-	// );
-
-	// const snapshot$ = derived(
-	// 	feeds$,
-	// 	(raw, set) => {
-	// 		if (raw.type === 'snapshot') {
-	// 			const data = parseSnapshot(raw);
-	// 			console.log(data);
-	// 			set(data);
-
-	// 			bids$.set(data.bids);
-	// 			asks$.set(data.asks);
-	// 		}
-
-	// 		if (raw.type?.includes('update')) {
-	// 			const data = {
-	// 				type: raw.type,
-	// 				product_id: raw.product_id,
-	// 				changes: raw.changes.map((change) => [
-	// 					change[0],
-	// 					parseFloat(change[1]),
-	// 					parseFloat(change[2])
-	// 				]),
-	// 				time: new Date(raw.time)
-	// 			} as Update;
-
-	// 			// sync changes
-	// 			data.changes.forEach((change) => {
-	// 				const type = change[0];
-	// 				if (type === 'buy') {
-	// 					bids$.update((val) => sync(val, change));
-	// 				} else {
-	// 					asks$.update((val) => sync(val, change));
-	// 				}
-	// 			});
-	// 		}
-	// 	},
-	// 	{ bids: [], asks: [], type: 'snapshot' } as Snapshot
-	// );
-
-	// const updates$ = derived<Readable<{}>, Update>(
-	// 	feeds$,
-	// 	(raw, set) => {
-	// 		if (raw.type?.includes('update')) {
-	// 			const data = {
-	// 				type: raw.type,
-	// 				product_id: raw.product_id,
-	// 				changes: raw.changes.map((change) => [
-	// 					change[0],
-	// 					parseFloat(change[1]),
-	// 					parseFloat(change[2])
-	// 				]),
-	// 				time: new Date(raw.time)
-	// 			} as Update;
-	// 			set(data);
-
-	// 			// sync changes
-	// 			data.changes.forEach((change) => {
-	// 				const type = change[0];
-	// 				if (type === 'buy') {
-	// 					bids$.update((val) => sync(val, change));
-	// 				} else {
-	// 					asks$.update((val) => sync(val, change));
-	// 				}
-	// 			});
-	// 		}
-	// 	},
-	// 	{ changes: [], type: '' }
-	// );
-
-	return { asks$, bids$ };
+	return { asks$, bids$, isClosed, reconnect, disconnect };
 }
 
 function parseSnapshot(raw: Record<string, any>): Snapshot {
