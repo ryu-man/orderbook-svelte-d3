ed9a6673dcaac9f57999b581c64235d0ed3f0bab
abdelhalim <rioman19@gmail.com>
2023-08-29 19:43:57 +0100

    clip canvas content

---
+++ src/lib/orderbook/ViewportClipper.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { Clip, getCanvasViewportContext } from '$lib/canvas';
+
+	const { vh$, vw$, vx$, vy$ } = getCanvasViewportContext();
+</script>
+
+<Clip x={$vx$} y={$vy$} width={$vw$} height={$vh$}>
+	<slot />
+</Clip>
